9006c430830e18f76ab05417530e49398785bf1d
maks-epowar <maks@epowar.com>
2024-08-08 14:41:13 +0100

    fix always permission not required when location update is started in foreground

--- ios/Classes/SwiftBackgroundLocationPlugin.swift
+++ ios/Classes/SwiftBackgroundLocationPlugin.swift
@@ -20,11 +20,11 @@ public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationM
         if (SwiftBackgroundLocationPlugin.locationManager == nil) {
             SwiftBackgroundLocationPlugin.locationManager = CLLocationManager()
             SwiftBackgroundLocationPlugin.locationManager?.delegate = self
-            SwiftBackgroundLocationPlugin.locationManager?.requestAlwaysAuthorization()
+            SwiftBackgroundLocationPlugin.locationManager?.requestWhenInUseAuthorization()
 
             SwiftBackgroundLocationPlugin.locationManager?.allowsBackgroundLocationUpdates = true
             if #available(iOS 11.0, *) {
-                SwiftBackgroundLocationPlugin.locationManager?.showsBackgroundLocationIndicator = true;
+                SwiftBackgroundLocationPlugin.locationManager?.showsBackgroundLocationIndicator = true
             }
             SwiftBackgroundLocationPlugin.locationManager?.pausesLocationUpdatesAutomatically = false
         }
